8b91e752d9d29f962d0f0475e391086e84fcbf9e
r04922170 <r04922170@linux11.csie.ntu.edu.tw>
2016-03-28 23:56:02 +0800

    + form input/scene* folders & + alignment to output/

--- main.cpp
+++ main.cpp
@@ -8,10 +8,14 @@ int main(int argc, char* argv[]) {
 	int i;
 
 	// check the arguments
-	// argv[1]: the directory name
+	// argv[1]: the input dir name
 	// argv[2]: the plain-text file name
-	if(argc != 3) {
+	// argv[3]: the output dir name
+	if(argc != 4) {
 		fprintf(stderr, "wrong argument!!\n");
+		fprintf(stderr, "Usage:\n");
+		fprintf(stderr, "\t./a.out <input dirname> <detail txt file> <output dirname>\n");
+		fprintf(stderr, "\n");
 		exit(-1);
 	}
 
@@ -27,27 +31,37 @@ int main(int argc, char* argv[]) {
 	}
 
 	Mat images[16];
+	char name[16][100];
+	char full_name[16][100];
 	i = 0;
 	while(!feof(fp)) {
-		char tmp_name[100];
-		char img_file_name[100];
 		int dummy;
-		fscanf("%s%d", tmp_name, &dummy);
-		fprintf(stderr, "getting the image: %s, with shutter speed: %d\n", tmp_name, dummy);
-		sprintf(img_file_name, "%s/%s", argv[1], tmp_name);
-		fprintf(stderr, "opening the image at: %s\n", img_file_name);
-		images[i++] = imread(img_file_name, CV_LOAD_IMAGE_UNCHANGED);
+		if(!fscanf(fp, "%s%d", name[i], &dummy))
+			break;
+		if(feof(fp))
+			break;
+		fprintf(stderr, "getting the image: %s, with shutter speed: %d\n", name[i], dummy);
+		sprintf(full_name[i], "%s/%s", argv[1], name[i]);
+		fprintf(stderr, "opening the image at: %s\n", full_name[i]);
+		images[i] = imread(full_name[i], CV_LOAD_IMAGE_UNCHANGED);
+		i += 1;
 		if(i == 16) break; // in case out of range
 	}
+	fclose(fp);
 
 	// do the alignment
-	AlignImages(images, i);
+	fprintf(stderr, "start aligning...\n");
+	AlignImages(i, images);
+	fprintf(stderr, "finish aligning...\n");
 	
 	// save the aligned images
 	for(int j=0; j<i; j++) {
 		char out_file_name[100];
-		sprintf(out_file_name, "%d.JPG", j+1);
+		sprintf(out_file_name, "%s/%s", argv[3], name[j]);
+		fprintf(stderr, "saving images[%d] as %s...\n", j, out_file_name);
 		imwrite(out_file_name, images[j]);
+		fprintf(stderr, "image saved.\n");
 	}
-
+	fprintf(stderr, "image alignment finished!\n");
+	return 0;
 }
